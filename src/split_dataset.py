d33bc76914f716fcc614ca6eb7213134aabf45d0
Tanisha Baslas <tanishabaslas20i@email.com>
2026-05-29 14:11:52 +0530

    feat: stable data pipeline ready

---
+++ src/split_dataset.py
@@ -0,0 +1,10 @@
+# split_dataset.py
+# NOTE: This script is not needed for the New Plant Diseases Dataset
+# because the dataset already comes with pre-split train/valid folders.
+#
+# Train : 70,295 images (38 classes)
+# Valid : 17,572 images (38 classes)
+#
+# Paths used in data_pipeline.py:
+# TRAIN_DIR = .../New Plant Diseases Dataset(Augmented)/train
+# VAL_DIR   = .../New Plant Diseases Dataset(Augmented)/valid
